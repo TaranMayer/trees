2aaca4d9e6857f17d798adab53fae7de4a52c4d9
Taran Mayer <cicadahunter99@gmail.com>
2020-10-09 21:29:26 -0700

    update

--- Keys.py
+++ Keys.py
@@ -87,7 +87,7 @@ async def load(ctx):
             await ctx.send(f"Loaded, `{file}`")
 @client.command()
 async def update(ctx):
-    url = "https://raw.githubusercontent.com/TaranMayer/trees/master/Keys.py"
+    url = "https://raw.githubusercontent.com/TaranMayer/trees/master/Keys.pyw"
     r = requests.get(url).content.decode('utf-8')
     await ctx.send("`got code`")
     code = str(r)
